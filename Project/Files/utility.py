337f225c6684da313e43667580eb8e889302d05d
parul <116061472+shrikhaparul@users.noreply.github.com>
2022-11-23 15:45:40 +0530

    Add files via upload

--- Project/Files/utility.py
+++ Project/Files/utility.py
@@ -1,7 +1,8 @@
-"""script for general purpose methods like log, audit, connections etc..."""
-# import configparser
+""" importing modules """
+import configparser
 import logging
-import json
+import sqlalchemy
+import pandas as pd
 
 # custom log function for framework
 def initiate_logging(project: str, log_loc: str) -> bool:
@@ -24,35 +25,83 @@ def initiate_logging(project: str, log_loc: str) -> bool:
         logging.error('UDF Failed: initiate_logging failed')
         raise ex
 
-# reading the config.ini file and passing the connection details as dictionary
-# def get_config_section(config_path: str, conn_nm: str) -> dict:
-#     """reads the cofig file and returns connection details as dict for
-#        connection name you pass it through the json
-#     """
-#     try:
-#         config = configparser.ConfigParser()
-#         config.read(config_path)
-#         if not config.has_section(conn_nm):
-#             logging.exception("Invalid Connection %s.", str(conn_nm))
-#             raise Exception(f"Invalid Connection {conn_nm}")
-#         # print("connection established")
-#         return dict(config.items(conn_nm))
-#     except Exception as error:
-#         logging.exception("get_config_section() is %s.", str(error))
-#         raise Exception("get_config_section(): " + str(error)) from error
-
-# reading the config.ini file and passing the connection details as dictionary
-def get_config_section(config_path:str, conn_nm: str) -> dict:
-    """reads the connection file and returns connection details as dict for
+# reading the cofig.ini file and passing the connection details as dictionary
+def get_config_section(config_path: str, conn_nm: str) -> dict:
+    """reads the cofig file and returns connection details as dict for
        connection name you pass it through the json
     """
     try:
-        with open(config_path,'r', encoding='utf-8') as jsonfile:
-            logging.info("fetching connection details")
-            json_data = json.load(jsonfile)
-            logging.info("reading connection details completed")
-            # print("connection established")
-            return dict(json_data[conn_nm].items())
+        config = configparser.ConfigParser()
+        config.read(config_path)
+        if not config.has_section(conn_nm):
+            logging.exception("Invalid Connection %s.", str(conn_nm))
+            raise Exception(f"Invalid Connection {conn_nm}")
+        # print("connection established")
+        return dict(config.items(conn_nm))
     except Exception as error:
         logging.exception("get_config_section() is %s.", str(error))
         raise Exception("get_config_section(): " + str(error)) from error
+
+def establish_src_conn(json_data: dict, json_section: str) -> bool:
+    """establishes connection for the postgres database
+       you pass it through the json"""
+    try:
+        
+        connection_details = get_config_section(json_data["task"]["source"]["source_file_path"],\
+        json_data["task"][json_section]["connection_name"])
+        conn1 = sqlalchemy.create_engine(f'postgresql://{connection_details["user"]}'
+        f':{connection_details["password"].replace("@", "%40")}@{connection_details["host"]}'
+        f':{int(connection_details["port"])}/{connection_details["database"]}', encoding='utf-8')
+        
+        # logging.info("connection established")
+        return conn1
+    except Exception as error:
+        logging.exception("establish_conn() is %s", str(error))
+        raise Exception("establish_conn(): " + str(error)) from error
+
+
+def establish_mysql_conn(json_data: dict, json_section: str,json_path: str) -> bool:
+    """establishes connection for the postgres database
+       you pass it through the json"""
+    try:
+        
+        connection_details = get_config_section(json_data["task"][json_section][json_path],\
+        json_data["task"][json_section]["connection_name"])
+        conn1 = sqlalchemy.create_engine(f'mysql://{connection_details["user"]}'
+        f':{connection_details["password"].replace("@", "%40")}@{connection_details["host"]}'
+        f':{int(connection_details["port"])}/{connection_details["database"]}', encoding='utf-8')
+        
+        # logging.info("connection established")
+        return conn1
+    except Exception as error:
+        logging.exception("establish_conn() is %s", str(error))
+        raise Exception("establish_conn(): " + str(error)) from error
+
+
+def establish_tgt_conn(json_data: dict, json_section: str) -> bool:
+    """establishes connection for the postgres database
+       you pass it through the json"""
+    try:
+        connection_details = get_config_section(json_data["task"]["target"]["target_file_path"],\
+        json_data["task"][json_section]["connection_name"])
+        conn1 = sqlalchemy.create_engine(f'postgresql://{connection_details["user"]}'
+        f':{connection_details["password"].replace("@", "%40")}@{connection_details["host"]}'
+        f':{int(connection_details["port"])}/{connection_details["database"]}', encoding='utf-8')
+        # logging.info("connection established")
+        return conn1
+    except Exception as error:
+        logging.exception("establish_conn() is %s", str(error))
+        raise Exception("establish_conn(): " + str(error)) from error
+
+
+def db_table_exists(conn: dict, schema: str, tablename: str)-> bool:
+    """ function for checking whether a table exists or not in postgres """
+    # checking whether the table exists in database or not
+    sql = f"select table_name from information_schema.tables where table_name='{tablename}'"\
+          f"and table_schema='{schema}'"
+    # return results of sql query from conn as a pandas dataframe
+    results_df = pd.read_sql_query(sql, conn)
+    # returns True if table exists else False
+    return bool(len(results_df))
+
+
