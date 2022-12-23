7f82b58bf0d9e9e46e4d9a8ad1326c763a150e79
parul <116061472+shrikhaparul@users.noreply.github.com>
2022-12-23 09:37:18 +0530

    Add files via upload

--- Project/Files/engine_code.py
+++ Project/Files/engine_code.py
@@ -9,55 +9,32 @@ import logging
 from datetime import datetime
 
 def download_files(Project_id,Task_id,path):
-    x=path+Task_id+".json"
-    print(x)
     with open(
-        r""+path+'Ingestion_Kart\\Pipeline\\Project\\'+Project_id+'\\Task\\'+Task_id+'\\'+Task_id+".json","r", encoding='utf-8') as jsonfile:
+        r""+path+'app/Program/Project/Pipeline/Task/'+Task_id+'/'+".json","r", encoding='utf-8') as jsonfile:
         config_json = json.load(jsonfile)
     source_conn_file = config_json['task']['source']['connection_name']
     target_conn_file = config_json['task']['target']['connection_name']
     source_type = config_json['task']['source']['source_type']
     target_type = config_json['task']['target']['target_type']
-    src_loc= "D:\\Ingestion_Kart\\Pipeline\\Project\\P_555"
-    trg_loc= config_json['task']['target']['target_file_path']
-    config_loc= config_json['task']['config_file']
-    print(source_conn_file,target_conn_file,source_type,target_type)
     
     
     #curl command for downloading the files
-    src_ini = 'curl -o '+path+'Ingestion_Kart\\Pipeline\\Project\\'+Project_id+'\\'+source_conn_file+'.json '\
+    src_json = 'curl -o '+path+'app/Common/Config'+'.json '\
     'https://raw.githubusercontent.com/shrikhaparul/Pipeline/main/Project/'+Project_id+'/'+source_conn_file+'.json'
-    trgt_ini = 'curl -o '+path+'Ingestion_Kart\\Pipeline\\Project\\'+Project_id+'\\'+target_conn_file+'.json '\
+    trgt_json = 'curl -o '+path+'app/Common/Config'+'.json '\
     'https://raw.githubusercontent.com/shrikhaparul/Pipeline/main/Project/'+Project_id+'/'+target_conn_file+'.json'
-    config_ini= 'curl -o '+path+'Ingestion_Kart\\Pipeline\\Project\\'+Project_id+'\\'+'config.ini '\
-    'https://raw.githubusercontent.com/shrikhaparul/Pipeline/main/Project/'+Project_id+'/'+'config.ini'
     
-    subprocess.call(src_ini)
-    subprocess.call(trgt_ini)
-    subprocess.call(config_ini)
     
-
+    subprocess.call(src_json)
+    subprocess.call(trgt_json)
     
-#     #connecting with github
-#     g = Github("shrikhaparul", "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")
-#     repo = g.get_user().get_repo('intelli_kart')
-    
-#     #reading git hub data
-#     mapping =repo.get_contents("mapping.json")
-    
-#     print(mapping)
-#     print(target_type)
-    
-#     y= mapping.decoded_content.decode()
-    
-#     data = json.loads(y)
   
-    mapping_file='curl -o '+path+'Ingestion_Kart\\Pipeline\\Project\\'+Project_id+'\\Task\\'+Task_id+'\\'+'mapping.json '\
+    mapping_file='curl -o '+path+'app/Common/Scripts/engine_main'+'mapping.json '\
     'https://raw.githubusercontent.com/shrikhaparul/Pipeline/main/Project/Files/mapping.json'
 
     subprocess.call(mapping_file)
 
-    with open(r""+path+'Ingestion_Kart\\Pipeline\\Project\\'+Project_id+'\\Task\\'+Task_id+'\\'+'mapping.json',"r", encoding='utf-8') as mapjson:
+    with open(r""+path+'app/Common/Scripts/engine_main'+'mapping.json',"r", encoding='utf-8') as mapjson:
         config_new_json = json.load(mapjson)
     
     
@@ -68,13 +45,13 @@ def download_files(Project_id,Task_id,path):
     print(QC_check_file,Utility_file)
     
     #curl command for downloading the files
-    src_py = 'curl -o '+path+'Ingestion_Kart\\Pipeline\\Project\\'+Project_id+'\\Task\\'+Task_id+'\\'+source_file_name+' '\
+    src_py = 'curl -o '+path+'app/Common/Scripts/ingestion'+' '\
     'https://raw.githubusercontent.com/shrikhaparul/pipeline/main/Project/Files/'+source_file_name
-    trgt_py = 'curl -o '+path+'Ingestion_Kart\\Pipeline\\Project\\'+Project_id+'\\Task\\'+Task_id+'\\'+target_file_name+' '\
+    trgt_py = 'curl -o '+path+'app/Common/Scripts/ingestion'+' '\
     'https://raw.githubusercontent.com/shrikhaparul/pipeline/main/Project/Files/'+target_file_name
-    QC_py= 'curl -o '+path+'Ingestion_Kart\\Pipeline\\Project\\'+Project_id+'\\Task\\'+Task_id+'\\'+QC_check_file+' '\
+    QC_py= 'curl -o '+path+'app/Common/Scripts/dq_scripts'+' '\
     'https://raw.githubusercontent.com/shrikhaparul/pipeline/main/Project/Files/'+QC_check_file
-    Utility_py= 'curl -o '+path+'Ingestion_Kart\\Pipeline\\Project\\'+Project_id+'\\Task\\'+Task_id+'\\'+Utility_file+' '\
+    Utility_py= 'curl -o '+path+'app/Common/Scripts/ingestion'+' '\
     'https://raw.githubusercontent.com/shrikhaparul/pipeline/main/Project/Files/'+Utility_file
     
     #calling the subprocess to run curl command function
@@ -86,23 +63,7 @@ def download_files(Project_id,Task_id,path):
 
 
 def engine_main(Project_id,Task_id,path):
-    from utility import initiate_logging
     import definitions_qc as dq
-    ## logging module
-    run_id='run_id_' + datetime.now().strftime("%Y%m%d%H%M%S%f") 
-    # new_path=path+'\\'
-    os.chdir(path)
-    if not os.path.exists('Run_id'):
-        os.makedirs('Run_id')
-    new_path= path  +'Run_id\\'+run_id+'\\'
-    os.chdir(path  +'Run_id\\')
-    os.makedirs(run_id)
-    initiate_logging(run_id,r""+new_path)
-    # print(new_path)
-    logging.info('execution started')
-    # initiate_logging('engine_log', r""+path)
-    logging.info('logging initiated')
-    # reading the json file
     try:
         with open(r""+path+Task_id+".json","r", encoding='utf-8') as jsonfile:
             logging.info("reading json data started")
@@ -112,10 +73,20 @@ def engine_main(Project_id,Task_id,path):
         logging.exception("error in reading json %s.", str(error))
         raise Exception("error in reading json: " + str(error)) from error
 
+    try:
+        with open(r""+path+"checks_mapping.json","r", encoding='utf-8') as json_data_new:
+            logging.info("reading json data started")
+            json_checks = json.load(json_data_new)
+            logging.info("reading json data completed")
+    except Exception as error:
+        logging.exception("error in reading json %s.", str(error))
+        raise Exception("error in reading json: " + str(error)) from error
+
+    
     # Precheck script execution starts here
     if json_data["task"]["source"]["source_type"] == 'csv_read' or json_data["task"]["source"]["source_type"] == 'postgres_read' or\
      json_data["task"]["source"]["source_type"] == 'mysql_read' or json_data["task"]["source"]["source_type"] == 'mssql_read':
-       pre_check = dq.qc_pre_check(json_data)
+       pre_check = dq.qc_pre_check(json_data, json_checks)
 
     
     #file conversion code and ingestion code
@@ -193,8 +164,8 @@ def engine_main(Project_id,Task_id,path):
     # postcheck script execution starts here
     if json_data["task"]["target"]["target_type"] == 'csv_write' or json_data["task"]["target"]["target_type"] == 'postgres_write' or \
       json_data["task"]["target"]["target_type"] == 'mysql_write' or json_data["task"]["target"]["target_type"] == 'mssql_write' :
-        #post check code
-        post_check = dq.qc_post_check(json_data)
+        # post check code
+        post_check = dq.qc_post_check(json_data, json_checks)
 
         #qc report generation
         qc_report = dq.qc_report(pre_check, post_check, json_data,new_path)
